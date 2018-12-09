70b0da11c33037701b7e7be6fb12a7d6aebf88e6
rdinizcal <rdinizcal@gmail.com>
2018-12-09 19:20:05 -0200

    classes extraction to record data structure

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ from scipy.spatial import distance
 from sklearn.cluster import KMeans
 
 from dtmc import *
+from record import *
 
 
 # TODO : method extraction
@@ -18,29 +19,6 @@ def getVal(index, str):
 	arr = str.split(",")
 	match = re.match("^[-+]?([0-9]+(\\.[0-9]+)?|\\.[0-9]+)$",arr[index])
 	return arr[index][match.start():match.end()]
-
-class Record : 
-
-    def __init__ (self, recordID, age, gender, height, icutype, weight):
-        self.recordID = recordID
-        self.age = age
-        self.gender = gender
-        self.height = height
-        self.weight = weight
-
-        icutype = int(icutype)
-        if (icutype == 1):
-            self.icutype = "Coronary Care Unit"
-        elif (icutype == 2):
-            self.icutype = "Cardiac Surgery Unit"
-        elif (icutype == 3):
-            self.icutype = "Medical ICU"
-        elif (icutype == 4):
-            self.icutype = "Surgical ICU"
-        else :
-            self.icutype = "unknown"
-        
-        self.vital_signals = {str: []}
 
 class Cluster :
 
